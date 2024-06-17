c044dda49b65309a94d23d8218c7c1656046c1aa
jmask <j.maskiewicz@student.uw.edu.pl>
2024-06-17 23:25:06 +0200

    try CNN instead FNN

--- agents/DDQN/DDQN_CNN.py
+++ agents/DDQN/DDQN_CNN.py
@@ -15,33 +15,36 @@ import pygame
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-class DuelingQNetwork(nn.Module):
-    def __init__(self, input_dims, n_actions, dropout_rate=1 / 5):
-        super(DuelingQNetwork, self).__init__()
-        self.fc1 = nn.Linear(input_dims, 1024)
+class DuelingQNetworkCNN(nn.Module):
+    def __init__(self, input_channels, n_actions, dropout_rate=0.2):
+        super(DuelingQNetworkCNN, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=8, stride=4)
         self.dropout1 = nn.Dropout(dropout_rate)
-        self.fc2 = nn.Linear(1024, 512)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.dropout2 = nn.Dropout(dropout_rate)
-        self.fc_value = nn.Linear(512, 512)
-        self.value = nn.Linear(512, 1)
-        self.fc_advantage = nn.Linear(512, 256)
-        self.advantage = nn.Linear(256, n_actions)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.dropout3 = nn.Dropout(dropout_rate)
+        self.fc1 = nn.Linear(64*7*7, 512)
+        self.dropout4 = nn.Dropout(dropout_rate)
+        self.fc_value = nn.Linear(512, 1)
+        self.fc_advantage = nn.Linear(512, n_actions)
 
     def forward(self, state):
-        x = torch.relu(self.fc1(state))
+        x = torch.relu(self.conv1(state))
         x = self.dropout1(x)
-        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.conv2(x))
         x = self.dropout2(x)
+        x = torch.relu(self.conv3(x))
+        x = self.dropout3(x)
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.fc1(x))
+        x = self.dropout4(x)
 
-        val = torch.relu(self.fc_value(x))
-        val = self.value(val)
-
-        adv = torch.relu(self.fc_advantage(x))
-        adv = self.advantage(adv)
+        val = self.fc_value(x)
+        adv = self.fc_advantage(x)
 
         q_values = val + (adv - adv.mean(dim=1, keepdim=True))
         return q_values
-
 class ReplayBuffer:
     def __init__(self, capacity):
         self.buffer = deque(maxlen=capacity)
